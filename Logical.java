e6ad49529304a0c798864d1b809e140988058e66
Pavithrakumaran <35188863+Pavithrakumaran@users.noreply.github.com>
2018-01-21 12:17:44 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,12 @@
+package pavi;
+
+public class Logical {
+
+	public static void main(String[] args) {
+		int a=5,b=6,c=1;
+		 System.out.println("the output is:"+(a++ + b--)*(c-- - a--));
+		// TODO Auto-generated method stub
+
+	}
+
+}
