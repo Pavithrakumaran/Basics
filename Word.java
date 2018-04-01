561c09297ed1696652ab1cc257c7aa777de491f9
Pavithrakumaran <35188863+Pavithrakumaran@users.noreply.github.com>
2018-04-01 07:29:06 +0530

    Add files via upload

---
+++ Word.java
@@ -0,0 +1,15 @@
+package pavithra;
+
+import java.util.Scanner;
+
+public class Word {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc=new Scanner(System.in);
+    int i=sc.nextInt();
+    System.out.println("a"+i);
+	}
+
+}
+
