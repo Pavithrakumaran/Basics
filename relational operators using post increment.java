9580a1e661a2653bcd6b02c3db9b8b22ac883817
Pavithrakumaran <35188863+Pavithrakumaran@users.noreply.github.com>
2018-04-09 18:23:10 +0530

    Update and rename Logical2.java to relational operators using post increment.java

---
+++ relational operators using post increment.java
@@ -0,0 +1,13 @@
+package pavi;
+public class Logical2 
+{
+public static void main(String[] args)
+{
+int a=5,b=2,c=6,d=10;
+boolean e=(a > b++)&&(c++ < d--);
+System.out.println("the output is:"+e);
+}
+}
+
+OUTPUT:
+the output is: True
